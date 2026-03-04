e232d7e55d9c4796bc9fbd3233bef9cf34ec900f
emerson-bl <emerson.cook457@students.cmcss.net>
2026-03-04 13:29:55 -0600

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,12 +1,7 @@
 print("The door is ahead. Whether you make it through depends on one thing - do you have the key?")
 has_key = True
 if has_key:
-    Click = ("You find the key laying around. You put the key through the hole.")
-    print(Click)
-    outcome = ("You may continue")
+    outcome = 'Click:' "You find the key laying around. You put the key through the hole. You may continue"
 else:
-    Doom = ("You could not find the key.")
-    print(Doom)
-    outcome = ("You are dead")
+    outcome = 'Doom:' "You could not find the key. You are dead"
 print(outcome)
-
