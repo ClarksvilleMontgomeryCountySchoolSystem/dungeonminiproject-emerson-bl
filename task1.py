333963d2db420f6ae80167df828a296053a22c42
emerson-bl <emerson.cook457@students.cmcss.net>
2026-03-04 13:26:28 -0600

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,11 +1,7 @@
 print("You wake up on a cold stone floor. Your torch may be lit - and that changes everything.")
 torch_lit =  True
 if torch_lit:
-    Flicker = ("you light the torch with a lighter.")
-    print(Flicker)
-    outcome = ("You may continue")
+    outcome = 'Flicker:' "You light the torch with a lighter. You may continue"
 else:
-    Doom = ("You couldnt find a lighter.")
-    print(Doom)
-    outcome = ("You are dead")
+    outcome = 'Doom:' "You couldnt find a lighter. You are dead"
 print(outcome)
