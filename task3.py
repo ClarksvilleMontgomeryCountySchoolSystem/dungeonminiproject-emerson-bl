d8051b86e225c28a1778a1a43cd3276062b18e8f
emerson-bl <emerson.cook457@students.cmcss.net>
2026-03-04 13:02:34 -0600

    Update task3.py

---
+++ task3.py
@@ -0,0 +1,11 @@
+print("You're in the corridor now. Somewhere ahead, a guard is patrolling.")
+guard_awake = True
+if guard_awake:
+    Shadow = ("Be careful...he's awake."
+    print(Shadow)
+    outcome = ("You were able to be quiet. You may contuine."
+else:
+    Doom = ("You were not quiet.")
+    print(Doom)
+    outcome = ("You are dead")
+print(outcome)
