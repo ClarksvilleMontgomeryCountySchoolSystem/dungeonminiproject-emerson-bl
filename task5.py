c0b7d4c721f3720c1e24292e1ffb439a3f12038f
emerson-bl <emerson.cook457@students.cmcss.net>
2026-03-05 10:40:27 -0600

    Update task5.py

---
+++ task5.py
@@ -0,0 +1,7 @@
+print("You're out. Or are you?")
+escaped = True
+if escaped:
+    outcome = 'Legend:' "You escape from the dungeon and are remebered as a hero for the rest of your days."
+else:
+    outcome = 'Doom:' "You get lost in the forest surrounding the dungeon. You are dead"
+print(outcome)
