eca76d464d4c4b98a4049a3e9fc7def47c4ee399
emerson-bl <emerson.cook457@students.cmcss.net>
2026-03-09 13:02:22 -0500

    Update task4.py

--- task4.py
+++ task4.py
@@ -1,9 +1,9 @@
 print("The exit is in sight. But the drawbridge - is it down?")
 drawbridge_raised = True
 if not drawbridge_raised:
-    outcome = 'Thunder:' "The drawbride seems to be raised. You may continue"
-else:
     outcome = 'Doom' "The drawbridge seems to be lowereed. The gaurd soon catches you. You are dead"
+else:
+    outcome = 'Thunder:' "The drawbride seems to be raised. You may continue"
 print(outcome)
 
 
